8bfd5c3356f8d09695d5d5e2d9b35136ecf8a3b4
abdiHaret <abdiharet68@gmail.com>
2023-12-20 20:56:57 -0500

    Added an if statement

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main(){
     printf("Hello World\n");
-    printf("Just double checking for something");
-    printf("Added another line for checking again");
+    printf("Just double checking for something\n");
+    printf("Added another line for checking again\n");
+    int n = 20;
+    if(n==20){
+        printf("Wiil wiil dhalay\n");
+    }
+    else{
+        printf("Waxan waa howl kale\n");
+    }
+
 }
